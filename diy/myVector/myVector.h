eddbd04fd1da4791349a6c52b4e1470aeb4a9ab3
zhangguangyu <zhangguangyu@ainemo.com>
2017-10-17 20:17:31 +0800

    add

--- diy/myVector/myVector.h
+++ diy/myVector/myVector.h
@@ -18,7 +18,7 @@ void destory(Tp first, Tp last){
 }
 
 template<typename T>
-class myVector{
+class MyVector{
 public:
     typedef T value_type;
     typedef T* iterator;
@@ -29,14 +29,14 @@ public:
     typedef const T& const_reference;
     typedef size_t size_type;
 
-    myVector();
-    myVector(size_type n, T value = T());
-    myVector(iterator begin, iterator end);
-    ~myVector();
+    MyVector();
+    MyVector(size_type n, T value = T());
+    MyVector(iterator begin, iterator end);
+    ~MyVector();
 
     // copy control
-    myVector(const myVector&);
-    myVector& operator=(const myVector&);
+    MyVector(const MyVector&);
+    MyVector& operator=(const MyVector&);
 
     // 任何不会修改数据成员的函数都应该声明为const 类型。
     // 如果在编写const 成员函数时，不慎修改了数据成员，或者调用了其它非const 成员函数，编译器将指出错误，这无疑会提高程序的健壮性。
@@ -66,5 +66,32 @@ protected:
 private:
     static std::allocator<T> alloc;
 };
+
+// static class member needed to be defined outside of class
+template<typename T>
+std::allocator<T> MyVector<T>::alloc;
+
+template<typename T>
+MyVector<T>::MyVector():start(NULL), finish(NULL), end_of_storage(NULL){}
+
+template<typename T>
+MyVector<T>::MyVector(size_type n, T value){
+    start = alloc.allocate(n); // 数量，分配空间
+    end_of_storage = finish = start + n;
+
+    for(iterator i = start; i!=finish; i++){
+        alloc.construct(i, value); // 构建
+    }
+}
+
+template<typename T>
+MyVector<T>::MyVector(iterator begin, iterator end){
+    // 指针相减，获得两个指针之间的元素数目
+    const size_type n = end - begin;
+    start = alloc.allocate(n); // 分配空间
+    finish = end_of_storage = start + n;
+    std::uninitialized_copy(begin, end, start); // 构建 等同for (; begin != end; ++begin, (void) ++start) ...
+}
+
 
 #endif // MY_VECTOR_H
